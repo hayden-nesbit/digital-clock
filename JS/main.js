de13f6ec036d597ac9e7588af46193db36dd8756
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-02-18 13:51:09 -0500

    clock ready

--- JS/main.js
+++ JS/main.js
@@ -3,10 +3,17 @@ var int = setInterval(myClock, 1000);
 
 function myClock() { 
     //This is the function that is run through the 1 second interval
-  var date = new Date(); 
-    //This date variable gets JavsScript's default current date and time
-  var time = date.toLocaleTimeString(); 
-    //This runs the current date and time and gets only the date
-  document.getElementById("clock").innerHTML = time; 
-    //This takes the time output and pushes it to my HTML where the "clock" class is
+    var date = new Date(); 
+        //This date variable gets JavsScript's default current date and time
+    var time = date.toLocaleTimeString();
+        //This runs the current date and time and gets only the date
+
+    document.getElementById("clock").innerHTML = time;
+        //This takes the time output and pushes it to my HTML where the "clock" class is
+
+    //The following lines run the same process, but give the date as output
+    var month = date.toLocaleDateString();
+
+    document.getElementById("month").innerHTML = month;
+
 }
